46aeaa6c0a99fbb3fe060d31af2c7429064f892c
Gabriel Alves <gabi.12.spin@hotmail.com>
2021-06-10 00:16:53 -0300

    Ajuste do cors

--- app.js
+++ app.js
@@ -7,30 +7,12 @@ const routeTask = require("./routes/task");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-var corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200, // For legacy browser support
-    methods: "PUT, POST, PATCH, DELETE, GET"
-}
-
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app
   .use(express.static(__dirname + "/public"))
-  .use(cors(corsOptions))
   .use(cookieParser());
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");  
-//   res.setHeader("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-
-//   next();
-// });
 
 app.use("/user", routeUser);
 app.use("/workflow", routeWorkflow);
