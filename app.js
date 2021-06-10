8e75371b6d3eb9381ee294d6ee588d65a5a45c78
Gabriel Alves <gabi.12.spin@hotmail.com>
2021-06-09 22:29:55 -0300

    Ajuste do cors

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ app
   .use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
     "Access-Control-Allow-Header",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
 
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).send({});
   }
 
